1f399f34feaac46139583dc25961a5b6806a77da
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-20 16:01:43 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,14 @@
 class Backer
-  
-  attr_reader :backed_projects, :name
-  
 
-  
+  attr_accessor :name, :backed_projects
+
   def initialize(name)
     @name = name
     @backed_projects = []
   end
-  
-  def back_project(name)
-    backed_projects << name
+
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
   end
-  
 end
